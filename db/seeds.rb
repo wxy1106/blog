ac1c9a5147ee8ebf8ddf2b8a55bc868e9a746a6f
root <root@localhost.localdomain>
2016-07-11 11:39:20 -0400

    初始配置

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #添加管理员
-admin = User.new username: 'admin', password: 'adminqwe', password_confirmation: 'adminqwe', email: '736698959@qq.com', admin: true, activation: 1
+admin = User.new username: 'admin', password: '123456', password_confirmation: '123456', email: '19763526@qq.com', admin: true, activation: 1
 puts admin.save! ? 'add admin success.' : 'add admin fail!'
 
 #添加一个默认分类
-category = Category.new name: '默认分类'
+category = Category.new name: 'category1'
 puts category.save! ? 'add category success.' : 'add category fail!'
